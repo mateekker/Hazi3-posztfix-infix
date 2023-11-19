2e9ca7d7e9638dccdb94c185138f8bc69b423d0b
Máté <Mt@LAPTOP-9EC9HS3H>
2023-11-19 23:02:25 +0100

    Add project files.

---
+++ Hazi3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazi3
+{
+    class Program
+    {
+        static string convert_to_infix(string s)
+        {
+            string result = "";
+            string muvelet = "+-*/^%";
+            Stack<string> v = new Stack<string>();
+
+            foreach (char c in s)
+            {
+                if (muvelet.Contains(c))
+                {
+                    string t1 = v.Pop();
+                    result = $"({v.Pop()}{c}{t1})";
+                    v.Push(result);
+                }
+                else if ("0123456789".Contains(c))
+                    v.Push(c.ToString());
+
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(convert_to_infix("23-"));
+            Console.WriteLine(convert_to_infix("59*"));
+            Console.WriteLine(convert_to_infix("14/"));
+            Console.WriteLine(convert_to_infix("69^"));
+            Console.WriteLine(convert_to_infix("11-"));
+            Console.WriteLine(convert_to_infix("34*"));
+
+        }
+    }
+}
